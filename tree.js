1fa648260c68f44fe2d1fce8e126f4d4ad1814bb
Odai Jubran <95129948+odai-dev@users.noreply.github.com>
2025-12-25 17:19:41 +0300

    implement insert method in Tree class

--- tree.js
+++ tree.js
@@ -1,36 +1,65 @@
 import Node
 
-from "./node";
+    from "./node";
 export default class Tree {
-    constructor(arr = []){
-        arr.sort((a,b) => a - b);
+    constructor(arr = []) {
+        arr.sort((a, b) => a - b);
         arr = [...new Set(arr)];
         this.root = this.buildTree(arr)
     }
-    
+
     buildTree(arr) {
         if (arr.length == 0) return null;
-        
-        let mid = Math.floor(arr.length/2);
+
+        let mid = Math.floor(arr.length / 2);
         const node = new Node(arr[mid]);
 
         node.left = this.buildTree(arr.slice(0, mid));
-        node.right = this.buildTree(arr.slice(mid+1))
+        node.right = this.buildTree(arr.slice(mid + 1))
 
         return node
     }
-    
+
+    insert(value) {
+        const node = new Node(value);
+        if (this.root == null) {
+            this.root = node;
+            return;
+        }
+
+        let current = this.root;
+        while (true) {
+            if (value > current.data) {
+                if (current.right == null) {
+                    current.right = node;
+                    return;
+                } else {
+                    current = current.right;
+                }
+            } else if (value < current.data) {
+                if (current.left == null) {
+                    current.left = node;
+                    return;
+                } else {
+                    current= current.left;
+                }
+            } else {
+                return;
+            }
+        }
+
+    }
 }
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
 };
