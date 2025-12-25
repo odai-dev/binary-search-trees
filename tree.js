d0243d807452d830a9cebb060129d60065f6b50c
Odai Jubran <95129948+odai-dev@users.noreply.github.com>
2025-12-25 17:02:35 +0300

    Implement Tree class with constructor and buildTree method

--- tree.js
+++ tree.js
@@ -1,3 +1,36 @@
+import Node
+
+from "./node";
 export default class Tree {
+    constructor(arr = []){
+        arr.sort((a,b) => a - b);
+        arr = [...new Set(arr)];
+        this.root = this.buildTree(arr)
+    }
+    
+    buildTree(arr) {
+        if (arr.length == 0) return null;
+        
+        let mid = Math.floor(arr.length/2);
+        const node = new Node(arr[mid]);
+
+        node.left = this.buildTree(arr.slice(0, mid));
+        node.right = this.buildTree(arr.slice(mid+1))
+
+        return node
+    }
     
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
